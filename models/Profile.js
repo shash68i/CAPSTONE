353e91fe63ba88f4fd3e0078ebedffd1e0f6df29
Shashank <theshashank6870@gmail.com>
2022-03-14 18:12:44 +0530

    Profile Model Added

---
+++ models/Profile.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+  first_name: {
+    type: String,
+  },
+  last_name: {
+    type: String,
+  },
+  image_url: {
+    type: String,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+  },
+  bio: {
+    type: String,
+  },
+  social: {
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("profile", ProfileSchema);
