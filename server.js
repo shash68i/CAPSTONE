7f64f19e560f31b2c505e788f124854864c7b745
shash68i <theshashank6870@gmail.com>
2022-04-20 11:10:58 +0530

    Added CORS

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require("path");
 
 const app = express();
 
-// const cors = require("cors");
+const cors = require("cors");
 
-// const corsOptions = {
-//   origin: "*",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 //Connect to Database
 connectDB();
